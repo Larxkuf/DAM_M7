dbffbc6109d753b2e3efb2a58a79a7431a3727d3
Laura <153616329+Larxkuf@users.noreply.github.com>
2024-10-24 02:24:31 +0200

    Add files via upload

---
+++ achus/Anima/src/index.jsx
@@ -0,0 +1,8 @@
+import "../global.css";
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import { Frame } from "./screens/Frame";
+
+const app = document.getElementById("app");
+const root = ReactDOMClient.createRoot(app);
+root.render(<Frame />);
---
+++ achus/Anima/src/screens/Frame/Frame.jsx
@@ -0,0 +1,204 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <img
+            className="loadingb"
+            alt="Loadingb"
+            src="https://c.animaapp.com/X5J2C9Hh/img/loadingb2@2x.png"
+          />
+
+          <img
+            className="img"
+            alt="Loadingb"
+            src="https://c.animaapp.com/X5J2C9Hh/img/loadingb3@2x.png"
+          />
+
+          <img
+            className="loadingb-2"
+            alt="Loadingb"
+            src="https://c.animaapp.com/X5J2C9Hh/img/loadingb1@2x.png"
+          />
+
+          <div className="overlap">
+            <div className="rectangle" />
+
+            <div className="div" />
+
+            <div className="rectangle-2" />
+
+            <div className="rectangle-3" />
+
+            <div className="rectangle-4" />
+
+            <div className="rectangle-5" />
+
+            <div className="rectangle-6" />
+
+            <div className="rectangle-7" />
+
+            <div className="rectangle-8" />
+
+            <img
+              className="bluetooth"
+              alt="Bluetooth"
+              src="https://c.animaapp.com/X5J2C9Hh/img/bluetooth-2@2x.png"
+            />
+
+            <div className="rectangle-9" />
+
+            <p className="text-wrapper">No hay conección a internet</p>
+
+            <div className="redes-disponibles"> Redes disponibles:</div>
+
+            <div className="text-wrapper-2">Wi-Fi (Wifi_GJ3J)</div>
+
+            <div className="text-wrapper-3">Wi-Fi (Wifi_GJ3J)</div>
+
+            <div className="text-wrapper-4">Wi-Fi (Wifi_GJ3J)</div>
+
+            <div className="text-wrapper-5">Segura</div>
+
+            <div className="text-wrapper-6">Conectada, Sin internet</div>
+
+            <div className="text-wrapper-7">Segura</div>
+
+            <img
+              className="inalambrico"
+              alt="Inalambrico"
+              src="https://c.animaapp.com/X5J2C9Hh/img/inalambrico-3@2x.png"
+            />
+
+            <img
+              className="inalambrico-2"
+              alt="Inalambrico"
+              src="https://c.animaapp.com/X5J2C9Hh/img/inalambrico-3@2x.png"
+            />
+
+            <img
+              className="inalambrico-3"
+              alt="Inalambrico"
+              src="https://c.animaapp.com/X5J2C9Hh/img/inalambrico-3@2x.png"
+            />
+
+            <div className="text-wrapper-8">Wifi_GJ3J</div>
+
+            <img
+              className="pint"
+              alt="Pint"
+              src="https://c.animaapp.com/X5J2C9Hh/img/pint-1@2x.png"
+            />
+
+            <img
+              className="sistema"
+              alt="Sistema"
+              src="https://c.animaapp.com/X5J2C9Hh/img/sistema-2@2x.png"
+            />
+
+            <img
+              className="update"
+              alt="Update"
+              src="https://c.animaapp.com/X5J2C9Hh/img/update-1@2x.png"
+            />
+
+            <img
+              className="wifi"
+              alt="Wifi"
+              src="https://c.animaapp.com/X5J2C9Hh/img/wifi-1@2x.png"
+            />
+
+            <img
+              className="games"
+              alt="Games"
+              src="https://c.animaapp.com/X5J2C9Hh/img/games-1@2x.png"
+            />
+
+            <div className="text-wrapper-9">Estado</div>
+
+            <div className="rectangle-10" />
+
+            <img
+              className="puerto-ethernet"
+              alt="Puerto ethernet"
+              src="https://c.animaapp.com/X5J2C9Hh/img/puerto-ethernet-1@2x.png"
+            />
+
+            <div className="rectangle-11" />
+
+            <img
+              className="inalambrico-4"
+              alt="Inalambrico"
+              src="https://c.animaapp.com/X5J2C9Hh/img/inalambrico-4@2x.png"
+            />
+
+            <div className="rectangle-12" />
+
+            <img
+              className="television-led"
+              alt="Television led"
+              src="https://c.animaapp.com/X5J2C9Hh/img/television-led-1@2x.png"
+            />
+
+            <img
+              className="no-wifi"
+              alt="No wifi"
+              src="https://c.animaapp.com/X5J2C9Hh/img/no-wifi-1@2x.png"
+            />
+
+            <img
+              className="red-global"
+              alt="Red global"
+              src="https://c.animaapp.com/X5J2C9Hh/img/red-global-1@2x.png"
+            />
+
+            <img
+              className="line"
+              alt="Line"
+              src="https://c.animaapp.com/X5J2C9Hh/img/line-3.svg"
+            />
+
+            <img
+              className="line-2"
+              alt="Line"
+              src="https://c.animaapp.com/X5J2C9Hh/img/line-3.svg"
+            />
+
+            <img
+              className="modo-avion"
+              alt="Modo avion"
+              src="https://c.animaapp.com/X5J2C9Hh/img/modo-avion-1@2x.png"
+            />
+
+            <div className="text-wrapper-10">Modo avión</div>
+
+            <div className="text-wrapper-11">Estado</div>
+
+            <p className="ethernet-no-hay">
+              <span className="span">Ethernet </span>
+
+              <span className="text-wrapper-12">(no hay)</span>
+            </p>
+
+            <div className="text-wrapper-13">Desactivado</div>
+
+            <img
+              className="estudiante"
+              alt="Estudiante"
+              src="https://c.animaapp.com/X5J2C9Hh/img/estudiante-1@2x.png"
+            />
+
+            <img
+              className="intimidad"
+              alt="Intimidad"
+              src="https://c.animaapp.com/X5J2C9Hh/img/intimidad-1@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
