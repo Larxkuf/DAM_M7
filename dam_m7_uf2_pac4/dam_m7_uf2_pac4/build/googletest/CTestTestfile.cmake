52156a2c736733d00cfe5b680e387750b6a72327
Laura <153616329+Larxkuf@users.noreply.github.com>
2025-02-23 23:39:04 +0100

    Add files via upload

---
+++ dam_m7_uf2_pac4/dam_m7_uf2_pac4/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/dam_m7_uf2_pac4/dam_m7_uf2_pac4/googletest
+# Build directory: E:/dam_m7_uf2_pac4/dam_m7_uf2_pac4/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ dam_m7_uf2_pac4/dam_m7_uf2_pac4/build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/dam_m7_uf2_pac4/dam_m7_uf2_pac4/googletest/googlemock
+# Build directory: E:/dam_m7_uf2_pac4/dam_m7_uf2_pac4/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
