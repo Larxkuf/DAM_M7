84699744b263766052cedcf9f03b65d0d0044c5b
Laura <153616329+Larxkuf@users.noreply.github.com>
2025-02-23 23:31:30 +0100

    Add files via upload

---
+++ dam_m7_uf2_pac4/build/CMakeFiles/game_exec.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_exec.dir/cardgame.cpp.obj"
+  "CMakeFiles/game_exec.dir/cardgame.cpp.obj.d"
+  "game_exec.exe"
+  "game_exec.exe.manifest"
+  "game_exec.pdb"
+  "libgame_exec.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
