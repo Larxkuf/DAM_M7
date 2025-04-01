56b0abc4a0c25cee8a0e18766206a872da86de16
Laura <153616329+Larxkuf@users.noreply.github.com>
2025-04-01 15:43:50 +0200

    Update PatrolState.cs

--- m7-unity/Assets/Scripts/PatrolState.cs
+++ m7-unity/Assets/Scripts/PatrolState.cs
@@ -2,7 +2,6 @@
 
 public class PatrolState : IenemyState
 {
-    //Inicializa el estado patrulla y el primer punto del array de coordenadas de desplazamiento.
     EnemyAI myEnemy;
     private int nextWayPoint = 0;
 
@@ -11,8 +10,6 @@ public class PatrolState : IenemyState
         myEnemy = enemy;
     }
 
-    //Coloca la luz del enemigo a verde y le mueve hacia una posición del array de coordenadas.
-    //A través del IF, va gestionando si ha llegado al punto de destino para hacerle cambiar a otro punto.
     public void UpdateState()
     {
         myEnemy.enemyColor.SetColor("_Color", Color.green);
@@ -27,26 +24,22 @@ public class PatrolState : IenemyState
         }
     }
 
-    //Detiene el avance del enemigo y se coloca en estado alerta.
     public void GoToAlertState()
     {
         myEnemy.navMeshAgent.isStopped = true;
         myEnemy.currentState = myEnemy.alertState;
     }
 
-    //Detiene el avance del enemigo y se coloca en estado ataque.
     public void GoToAttackState()
     {
         myEnemy.navMeshAgent.isStopped = true;
         myEnemy.currentState = myEnemy.attackState;
     }
 
-    //No hace nada porque ya está en modo patrulla.
     public void GoToPatrolState()
     {
     }
 
-    //En caso de que algo entre en el collider y se mantenga, y el enemigo esté en modo patrulla, pasará a modo alerta.
     public void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Player")
